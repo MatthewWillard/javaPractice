7da1c956b873ad8fab651c5f1db6822d3cf34c09
Matthew Willard <MWillard85@gmail.com>
2020-01-13 17:30:11 -0700

    new example

--- testing.java
+++ testing.java
@@ -17,3 +17,28 @@ public class Prime {
             System.out.println(num + " is not a prime number.");
     }
 }
+
+
+/* CallingMethodsInSameClass.java
+ *
+ * illustrates how to call static methods a class
+ * from a method in the same class
+ */
+
+public class CallingMethodsInSameClass
+{
+	public static void main(String[] args) {
+		printOne();
+		printOne();
+		printTwo();
+	}
+
+	public static void printOne() {
+		System.out.println("Hello World");
+	}
+
+	public static void printTwo() {
+		printOne();
+		printOne();
+	}
+}
